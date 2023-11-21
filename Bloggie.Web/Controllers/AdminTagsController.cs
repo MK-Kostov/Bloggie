ad4ea3a5882db01c7fb37d695cf59cb1862238c8
Mihail Kostov <Mihail Kostov@ENTSOFTSOL-MIHA>
2023-11-21 14:28:27 -0500

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,119 @@
+﻿using Azure;
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Controllers
+{
+	public class AdminTagsController : Controller
+	{
+		private readonly ITagRepository tagRepository;
+
+		public AdminTagsController(ITagRepository tagRepository)
+		{
+			this.tagRepository = tagRepository;
+		}
+
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ActionName("Add")]
+		//public  IActionResult Add(AddTagRequest addTagRequest)
+		public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+		{
+			// Mapping AddTagRequest to Tag domain model
+			var tag = new Tag
+			{
+				Name = addTagRequest.Name,
+				DisplayName = addTagRequest.DisplayName,
+			};
+
+			//bloggieDbContext.Tags.Add(tag);
+			await tagRepository.AddAsync(tag);
+
+			return RedirectToAction("List");
+		}
+
+		[HttpGet]
+		[ActionName("List")]
+		//public  IActionResult List()
+		public async Task<IActionResult> List()
+		{
+			// use dbContext to read the tags
+			//var tags = bloggieDbContext.Tags.ToList();
+			var tags = await tagRepository.GetAllAsync();
+
+			return View(tags);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(Guid id)
+		{
+			// 1st method
+			//var tag = bloggieDbContext.Tags.Find(id);
+
+			//2nd method
+			var tag = await tagRepository.GetAsync(id);
+
+			if (tag != null)
+			{
+				var editTagRequest = new EditTagRequest
+				{
+					ID = tag.ID,
+					Name = tag.Name,
+					DisplayName = tag.DisplayName
+				};
+
+				return View(editTagRequest);
+			}
+
+			return View(null);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+		{
+			var tag = new Tag
+			{
+				ID = editTagRequest.ID,
+				Name = editTagRequest.Name,
+				DisplayName = editTagRequest.DisplayName
+			};
+
+			var existingTag = await tagRepository.UpdateAsync(tag);
+
+			if (existingTag != null)
+			{
+				return RedirectToAction("List");
+			}
+			else
+			{
+				return RedirectToAction("Edit", new { id = editTagRequest.ID });
+			}
+
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(Guid id)
+		{
+			var deleteTag = await tagRepository.DeleteAsync(id);
+
+			if (deleteTag != null)
+			{
+				return RedirectToAction("List");
+			}
+
+			//show an error notification
+			return View(null);
+		}
+	}
+}
---
+++ Bloggie.Web/Repositories/TagRepository.cs
@@ -0,0 +1,62 @@
+﻿using Azure;
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Repositories
+{
+	public class TagRepository : ITagRepository
+	{
+		private readonly BloggieDbContext bloggieDbContext;
+
+		public TagRepository(BloggieDbContext bloggieDbContext)
+		{
+			this.bloggieDbContext = bloggieDbContext;
+		}
+		public async Task<Tag> AddAsync(Tag tag)
+		{
+			await bloggieDbContext.Tags.AddAsync(tag);
+			bloggieDbContext.SaveChanges();
+			return tag;
+		}
+
+		public async Task<Tag?> DeleteAsync(Guid id)
+		{
+			var existingTag = await bloggieDbContext.Tags.FirstOrDefaultAsync(x => x.ID == id);
+
+			if (existingTag != null)
+			{
+				bloggieDbContext.Tags.Remove(existingTag);
+				await bloggieDbContext.SaveChangesAsync();
+				return existingTag;
+			}
+
+			return null;
+		}
+
+		public async Task<IEnumerable<Tag>> GetAllAsync()
+		{
+			return await bloggieDbContext.Tags.ToListAsync();
+		}
+
+		public async Task<Tag?> GetAsync(Guid id)
+		{
+			return await bloggieDbContext.Tags.FirstOrDefaultAsync(x => x.ID == id);
+		}
+
+		public async Task<Tag?> UpdateAsync(Tag tag)
+		{
+			var existingTag = await bloggieDbContext.Tags.FirstOrDefaultAsync(x => x.ID == tag.ID);
+			if (existingTag != null)
+			{
+				existingTag.Name = tag.Name;
+				existingTag.DisplayName = tag.DisplayName;
+
+				// save changes
+				await bloggieDbContext.SaveChangesAsync();
+				return existingTag;
+			}
+			return null;
+		}
+	}
+}
