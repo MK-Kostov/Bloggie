387a404f160f79b0ca39b15120bbd83ab9868b71
Mihail Kostov <mishoka.kostov@gmail.com>
2023-12-21 10:39:52 -0500

    Domain Model For Like Functionality and Migration

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -4,12 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Data
 {
-    public class BloggieDbContext : DbContext
-    {
-        public BloggieDbContext(DbContextOptions<BloggieDbContext> options) : base(options)
-        {
-        }
-        public  DbSet<BlogPost>BlogPost { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-    }
+	public class BloggieDbContext : DbContext
+	{
+		public BloggieDbContext(DbContextOptions<BloggieDbContext> options) : base(options)
+		{
+		}
+		public DbSet<BlogPost> BlogPost { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+		public DbSet<BlogPostLike> BlogPostLike { get; set; }
+	}
 }
