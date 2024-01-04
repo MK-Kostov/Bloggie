d0de30cad7e6709c28331c37215ee031974a12d2
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-04 09:09:56 -0500

    Delete User

--- Bloggie.Web/Controllers/AdminUsersController.cs
+++ Bloggie.Web/Controllers/AdminUsersController.cs
@@ -74,5 +74,22 @@ namespace Bloggie.Web.Controllers
 
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(Guid Id)
+		{
+			var user = await userManager.FindByIdAsync(Id.ToString());
+
+			if (user is not null)
+			{
+				var identityResult = await userManager.DeleteAsync(user);
+
+				if (identityResult is not null && identityResult.Succeeded)
+				{
+					return RedirectToAction("List", "AdminUsers");
+				}
+			}
+			return View();
+		}
 	}
 }
